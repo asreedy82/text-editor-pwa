29777783e125a9c6c2710d33e1d50661bc3aa444
areedy <a.reedy@gmail.com>
2023-02-26 21:03:07 -0500

    adding workbox plugins, manifest file, and css loaders to webpack config

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,21 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('updating database');
+
+  const jateDb = await openDB('jate',1);
+
+  const transaction = jateDb.transaction('jate', 'readwrite');
+
+  const store = transaction.objectStore('jate');
+
+  const textAdd = store.add(content);
+
+  const result = await request;
+  console.log('Text data saved to the database', result);
+
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
