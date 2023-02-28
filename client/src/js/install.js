3451108a56ce29aba973aaef366cb58c63ff5cc8
areedy <a.reedy@gmail.com>
2023-02-27 21:55:38 -0500

    fixed the install button

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -9,6 +9,7 @@ window.addEventListener('beforeinstallprompt', (event) => {
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
+    console.log('did this work');
     const promptEvent = window.deferredPrompt;
 
     if (!promptEvent) {
